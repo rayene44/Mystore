1e2fea05ffd8aa5b04c55acfc5e526863d6abeaa
rayene44 <khalsirayene12@gmail.com>
2025-03-30 01:35:15 +0100

    Update store.js

--- store.js
+++ store.js
@@ -9,3 +9,19 @@ function annuler(){
     document.getElementById("menu1").style.display="none"
     document.getElementById("svg5").style.display="none"
 }
+function recherche(){
+    document.getElementById("svg1").style.display="none"
+    document.getElementById("svg2").style.display="none"
+    document.getElementById("svg3").style.display="none"
+     document.getElementById("svg4").style.display="none"
+     document.getElementById("rech").style.display="flex"
+     document.getElementById("svg6").style.display="flex"
+}
+function annuler2(){
+    document.getElementById("svg1").style.display="flex"
+    document.getElementById("svg2").style.display="none"
+    document.getElementById("svg3").style.display="flex"
+     document.getElementById("svg4").style.display="flex"
+     document.getElementById("rech").style.display="none"
+     document.getElementById("svg6").style.display="none"
+}
