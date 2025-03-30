db4826eab2f333466c2ff3c21d6dff51e5f5459a
rayene44 <khalsirayene12@gmail.com>
2025-03-30 01:02:59 +0100

    Add files via upload

---
+++ store.js
@@ -0,0 +1,11 @@
+function menu(){
+    document.getElementById("svg1").style.display="none"
+    document.getElementById("menu1").style.display="flex"
+    document.getElementById("svg5").style.display="flex"
+    document.getElementById("body").style.position="fixed"
+}
+function annuler(){
+    document.getElementById("svg1").style.display="flex"
+    document.getElementById("menu1").style.display="none"
+    document.getElementById("svg5").style.display="none"
+}
